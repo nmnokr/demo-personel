8a15212993dc0a4ab219da3bea09dd03e250c1cb
NUMAN <NUMAN>
2021-01-23 00:18:35 +0300

    "post" operation completed

--- demo-personel/src/main/java/com/example/demo/personel/resource/personelController.java
+++ demo-personel/src/main/java/com/example/demo/personel/resource/personelController.java
@@ -3,9 +3,13 @@ package com.example.demo.personel.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.personel.model.personel;
@@ -16,8 +20,14 @@ public class personelController {
 	@Autowired
 	private PersonelRepository repository;
 
-	@GetMapping("/deneme")
+	@GetMapping("/getall")
 	public List<personel> getAll() {
 		return repository.findAll();
 	}
+ 
+	@RequestMapping(value = "/adda", method = RequestMethod.POST)
+    public ResponseEntity<String> postCharacter(@RequestBody personel per) {
+		repository.save(per);
+        return new ResponseEntity<>("Kayıt İşlemi Başarılı!", HttpStatus.OK);
+    }
 }
